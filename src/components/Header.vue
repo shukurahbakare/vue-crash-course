6785140cc1478da2300c178ae9d93b09a833f500
Shukura <bakaresukurah@outlook.com>
2024-06-11 00:01:42 +0100

    router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,9 @@
     <header>
         <h1>{{title}}</h1>
         <Button 
+            v-show="homePage"
             @btn-click="$emit('toggle-add-task')" 
-            :text="showAddTask ? 'Close' : 'Add Here' " 
+            :text="showAddTask ? 'Close' : 'Add Task' " 
             :color="showAddTask ? 'red' : 'green'"
             />
     </header>
@@ -22,6 +23,15 @@ export default {
   },
     components: {
         Button
+    },
+    computed: {
+        homePage() {
+            if(this.$route.path === '/'){
+                return true
+            }else {
+                return  false
+            }
+        }
     }
 }
 </script>
